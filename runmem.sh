193a362d9eae2b12c4cdabfc250689ff856328c2
Ryo <ryo@perf.cs.tut.ac.jp>
2019-10-02 15:26:48 +0900

    create and update util shell script

--- runmem.sh
+++ runmem.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-command="build/dynamorio/bin64/drrun -c build/lib/libmemtrace.so -- "$*
+command="build/dynamorio/bin64/drrun -c build/lib/libmemtrace.so test -- "$*
 # echo $command
 eval $command
