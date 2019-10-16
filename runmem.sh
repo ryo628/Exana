87d39fc46c7812273de3ff36077385e1d52556e9
Ryo <ryo@perf.cs.tut.ac.jp>
2019-10-16 15:36:15 +0900

    add mesure execute time code

--- runmem.sh
+++ runmem.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-command="build/dynamorio/bin64/drrun -c build/lib/libmemtrace.so test -- "$*
+command="build/dynamorio/bin64/drrun -c build/lib/libmemtrace.so -- "$*
 # echo $command
+t1=`date +%s.%3N` 
 eval $command
+t2=`date +%s.%3N`
+res=`echo "scale=1; ${t2}-${t1}" | bc` 
+echo "execute time ${res} s"
