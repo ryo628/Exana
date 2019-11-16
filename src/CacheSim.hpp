03e10321af9fdfa92bb40fb3535b578e4c40a6e4
Ryo <ryo@perf.cs.tut.ac.jp>
2019-11-16 17:35:44 +0900

    refactoring CacheSim.cpp

--- src/CacheSim.hpp
+++ src/CacheSim.hpp
@@ -12,7 +12,7 @@
 
 class CacheSim{
     private:
-        const std::string fname;
+        std::string fname;
         CacheMemory *cl1, *cl2, *cl3;
         long long int access, l1miss, l2miss, l3miss, l1hits, l2hits, l3hits;
         std::list<uint64_t> addrList;
@@ -22,7 +22,7 @@ class CacheSim{
         CacheSim(std::string, std::string);
         ~CacheSim();
         void run();
-        void openFile();
+        bool openFile();
         void checkAddr(uint64_t);
         void printResult();
 };
