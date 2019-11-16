b648e09abfea2d7a339688591089aa630a4f53d0
Ryo <ryo@perf.cs.tut.ac.jp>
2019-11-16 17:32:17 +0900

    create & update util shellscript

--- runmem.sh
+++ runmem.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+#DYNAMORIO_HOME="$(cd $(dirname $0) && pwd)/dynamorio"
+#eval "export DYNAMORIO_HOME=${DYNAMORIO_HOME}"
+
+eval "rm ./tmp/*"
+t1=`date +%s.%3N`
 command="build/dynamorio/bin64/drrun -c build/lib/libmemtrace.so -- "$*
 # echo $command
-t1=`date +%s.%3N` 
 eval $command
 t2=`date +%s.%3N`
 res=`echo "scale=1; ${t2}-${t1}" | bc` 
